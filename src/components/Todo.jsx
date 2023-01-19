6f1a82c62d541e2c53925dd55e929679b04eeded
Basheer Rajeh <muhammadbasheerrajeh@gmail.com>
2023-01-19 04:59:45 +0300

    Create a fake todolist and pass it to Todo componenet and render it

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,7 @@
-const Todo = () => {
+const Todo = ({ todo }) => {
     return (
         <div className="row d-flex justify-content-between align-content-center">
-            <p className="col">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, quia?
-            </p>
+            <p className="col">{todo.task}</p>
             <div className="col d-flex justify-content-end">
                 <button className="btn btn-outline-danger btn-sm">X</button>
             </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import Todo from "./Todo";
 
-
 const TodoList = () => {
+    const [todos] = useState([
+        { id: 1, task: "Buy groceries" },
+        { id: 2, task: "Finish report" },
+        { id: 3, task: "Call Mom" },
+        { id: 4, task: "Workout" },
+        { id: 5, task: "Pay bills" },
+        { id: 6, task: "Book dentist appointment" },
+        { id: 7, task: "Watch movie" },
+        { id: 8, task: "Read for an hour" },
+        { id: 9, task: "Plan weekend trip" },
+        { id: 10, task: "Organize closet" },
+    ]);
     return (
         <ul className="list-group">
-            <li className="list-group-item"><Todo /></li>
-            <li className="list-group-item"><Todo /></li>
-            <li className="list-group-item"><Todo /></li>
-            <li className="list-group-item"><Todo /></li>
+            {todos.map((todo) => (
+                <li key={todo.id} className="list-group-item">
+                    <Todo todo={todo} />
+                </li>
+            ))}
         </ul>
     );
-}
+};
 
 export default TodoList;
